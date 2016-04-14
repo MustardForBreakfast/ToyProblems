30190b61850e0b17b01090eb245789ba6f989828
Justin Wayne <wayne.jch@gmail.com>
2016-04-14 11:17:05 -0500

    Completed linkedList.js

---
+++ linkedList.js
@@ -0,0 +1,67 @@
+  // Implement a linked list using Pseudoclassical style.
+
+  var LinkedList = function(initialValue){
+    // A constructor function for linkedLists, which will themselves be comprised
+    // of "node" objects.
+    if (initialValue){
+      var newNode = this.createNode(initialValue);
+      this.tail = newNode;
+      this.head = newNode;
+    } else {
+      this.tail = null;
+      this.head = null;
+    }
+  }
+  
+  LinkedList.prototype = {
+    
+    addToTail : function(value){
+      // Adds a new node to the list's tail and updates
+      // all relevant pointers.
+      var newNode = this.createNode(value);
+      var oldTail = this.tail;
+      
+      if (oldTail){
+        oldTail.next = newNode;
+      } else {
+        this.head = newNode;
+      }
+      this.tail = newNode;
+    },
+    
+    removeHead : function(){
+      // Pops the head node off from the front of the list.
+      var oldHead = this.head;
+      this.head = oldHead.next;
+      
+      // Nullifies the tail in cases where the last
+      // node has been removed from the list.
+      if (this.head === null){
+        this.tail = null;
+      }
+    }, 
+    
+    contains : function(value){
+      // Iterates from head to tail checking for the presence
+      // of a searched value. Returns a bool accordingly.
+      var contained = false;
+      var current = this.head;
+      while (current){
+        if (current.value === value){
+          contained = true;
+          return contained;
+        } else {
+          current = current.next;
+        }
+      }
+      return contained;
+    },
+    
+    createNode : function(value){
+      // Generates a new node object with a given value.
+      return {
+        'value' : value,
+        'next' : null,
+      }
+    }
+  }
